914fcadae09ded5d5ce012ff3f787d611eef794f
Bhavin R Shah <bhavin.shah@synergytechservices.com>
2018-06-20 19:38:21 +0530

    Initial commit of work

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/regression_template.R
@@ -0,0 +1,47 @@
+# Regression template
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting the Regression Model to the dataset
+# Create the regressor here
+
+
+# Pedicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Regression Model results
+library(ggplot)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Regression Model results (for Higher resolution and smoother curve)
+library(ggplot)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            color = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
